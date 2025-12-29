801e898410086ff4a7584a3f39a28e74f0b37405
Mohib Abbas <abbasmohib222@gmail.com>
2025-12-29 02:24:58 -0500

    Create initial schema and start with adding df_customers and df_orders rables

--- sql/01_database.sql
+++ sql/01_database.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE ecomm_analytics_db;
 
+
--- sql/02_schema.sql
+++ sql/02_schema.sql
@@ -1,2 +1,39 @@
--- Active: 1766788582550@@127.0.0.1@5432@ecomm_analytics_db
+-- 02_schema.sql
+-- Purpose: create schema + core tables for multi-table ecommerce analytics
+
+-- keep everything in one schema for simplicity and core tables 
+
+CREATE SCHEMA IF NOT EXISTS ecomm_analytics;
+
+SET SEARCH_PATH to ecomm_analytics;
+
+-- Drop the table if it already exists so this script can be safely re-run from scratch
+
+DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+
+-- Customers table
+
+CREATE TABLE df_customers (
+    customer_id VARCHAR PRIMARY KEY,
+    customer_zip_code_prefix INTEGER,
+    customer_city VARCHAR,
+    customer_state VARCHAR
+);
+
+-- Orders table (each row is an order placed by a customer)
+
+CREATE TABLE df_Orders (
+  order_id                      VARCHAR(50) PRIMARY KEY,
+  customer_id                   VARCHAR(50),
+  order_status                  VARCHAR(30),
+  order_purchase_timestamp      TIMESTAMP,
+  order_approved_at             TIMESTAMP,
+  order_delivered_timestamp     TIMESTAMP,
+  order_estimated_delivery_date DATE
+);
+
 
